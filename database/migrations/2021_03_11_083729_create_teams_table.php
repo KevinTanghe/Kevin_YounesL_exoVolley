2e3f84987fe3298241f3fcffd3fcf5059c4db955
Tanghe Kevin <tanghekevin1@gmail.com>
2021-03-11 09:58:03 +0100

    first commit

---
+++ database/migrations/2021_03_11_083729_create_teams_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTeamsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('teams', function (Blueprint $table) {
+            $table->id();
+            $table->string('nom');
+            $table->string('city');
+            $table->string('country');
+            $table->string('numberMax');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('teams');
+    }
+}
